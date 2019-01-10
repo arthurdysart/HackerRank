2835603c3da78ca85a963069b754b8cc7aedd5df
Arthur Dysart <github@arthurdys.art>
2019-01-10 11:45:47 -0500

    add solution 'jumping_on_clouds'

--- programming/interview_prep_kit/warm_up/jumping_on_clouds/python_source.py
+++ programming/interview_prep_kit/warm_up/jumping_on_clouds/python_source.py
@@ -3,6 +3,7 @@
 HackerRank - Jumping on the Clouds
 https://www.hackerrank.com/challenges/jumping-on-the-clouds
 
+Updated on Thu Jan 10 11:44:05 2019
 Created on Mon Nov 12 21:23:14 2018
 @author: Arthur Dysart
 """
@@ -14,6 +15,52 @@ import sys
 
 ## MODULE DEFINITIONS
 class Solution:
+    """
+    Iteration over all elements in array.
+    Implements Greedy Algorithm.
+
+    Time complexity: O(n)
+        - Traverse array with at least one single-space jump
+    Space complexity: O(1)
+        - Evaluate constant number of pointers
+    """
+
+    def jump_clouds(self, n, a):
+        """
+        Determine minimum number of jumps to traverse array.
+
+        :param int n: number of elements in array
+        :param list[int] a: array of safe and dangerous spaces
+        :return: minimum number of movements to traverse array
+        :rtype: int
+        """
+        if not a:
+            return 0
+        
+        i = 0
+        p = 0
+
+        # Evaluate all moves until at most 2 spaces from last space
+        while i + 2 < n:
+
+            if a[i + 2] == 0:
+                # Move 2 spaces in single jump
+                i += 2
+
+            else:
+                # Move 1 space in single jump
+              i += 1
+
+            p += 1
+
+        if i + 1 == n:
+            # Final move has been completed
+            return p
+        else:
+            # Final move requires single 1 or 2 space jump
+            return p + 1
+
+class Solution2:
     """
     Iteration over all characters in array.
     Time complexity: O(n)
